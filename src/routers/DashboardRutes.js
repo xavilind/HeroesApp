0d11eb1eef0331aa478da5ffbe33cf1a8ece3357
xavilind <xavilind7@gmail.com>
2022-03-21 16:29:06 -0600

    Lista Heroes

--- src/routers/DashboardRutes.js
+++ src/routers/DashboardRutes.js
@@ -12,16 +12,16 @@ export const DashboardRutes = () => {
   return (
     <>
         <Navbar/>
+        <div className='container'>
+             <Routes>
+                     <Route path="marvel" element={<MarvelScreen />} />
+                 <Route path="dc" element={<DcScreen />} />
 
-        <Routes>
-            <Route path="marvel" element={<MarvelScreen />} />
-            <Route path="dc" element={<DcScreen />} />
-
-            <Route path="search" element={<SearchScreen/>} />
-            <Route path="hero" element={<HeroScreen/>} />
-
+                 <Route path="search" element={<SearchScreen/>} />
+                 <Route path="hero" element={<HeroScreen/>} />
             <Route path="/" element={<MarvelScreen/>} />
         </Routes>   
+        </div>
     </>
   )
 }
