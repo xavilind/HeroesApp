7a7ba513f625d6d71d5421801fb03633fa56dd8e
xavilind <xavilind7@gmail.com>
2022-03-21 16:10:17 -0600

    useNavigate

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,7 +1,24 @@
+import { useNavigate } from "react-router-dom";
 export const LoginScreen = () => {
-    return (
-      <div>
+    
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/marvel', {
+      replace: true
+    })
+  }
+  return (
+      <div className="container mt-5">
           <h1>LoginScreen</h1>
+          <hr />
+
+                  <button 
+                     className="btn btn-primary"
+                  onClick={ handleLogin }
+>
+                    Login
+                    </button>
       </div>
     )
   }
